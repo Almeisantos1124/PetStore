45b21619ab1aa46a19405327259535c01874c3a3
Vlsantos1124 <almeisantos@hotmail.com>
2022-01-25 17:52:16 -0300

    Criei o teste para fazer uma inclusão de registro script incluirPet(post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -2,6 +2,7 @@
 package petstore;
 // 2- Bibliotecas
 
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -9,29 +10,29 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 
 // 3 -Classe
 public class Pet {
     // 3.1 Atributos
-
     String uri ="https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
-
 
     // 3.2 Metodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
             return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    //incluir  - create - Post
-    @Test  //Identifica o metodo de test para o TestNG
+    //Incluir  - Create - Post
+    @Test  //Identifica o metodo ou função de test para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson ("db/pet1.json");
 
     // Sintaxe Gherkin
-    // Giv
-    // When
-   //  Then
+    // Giv (Dado)
+    // When (Quando)
+   //  Then (Então)
 
     given() //Dado
             .contentType("application/json")//conteudo em API REST
@@ -42,7 +43,10 @@ public class Pet {
     .then() // Então
             .log().all()
             .statusCode(200)
-
+            .body("name",is("Nutella"))
+            .body("status",is("available"))
+            .body("category.name", is("cachorro"))
+            .body("tags.name",contains("adocao"))
     ;
 
     }
